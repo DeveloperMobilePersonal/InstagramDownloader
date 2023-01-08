2bc65f0c37779866b8093ec09a915ba8ff6de029
NguyenTinh Developer Personal <acc.mcc22@gmail.com>
2023-01-08 09:14:28 -0800

    - Thêm ID Admob

--- app/src/main/java/instagram/photo/video/downloader/story/saver/unit/UnitId.kt
+++ app/src/main/java/instagram/photo/video/downloader/story/saver/unit/UnitId.kt
@@ -1,6 +1,6 @@
 package instagram.photo.video.downloader.story.saver.unit
 
-const val ID_MAIN_BANNER = ""
-const val ID_MAIN_INTER = ""
-const val ID_APPLICATION_APP_OPEN = ""
-const val ID_SPLASH_INTER = ""
+const val ID_MAIN_BANNER = "ca-app-pub-4211508479172397/4744786569"
+const val ID_MAIN_INTER = "ca-app-pub-4211508479172397/4434876949"
+const val ID_APPLICATION_APP_OPEN = "ca-app-pub-4211508479172397/8353940657"
+const val ID_SPLASH_INTER = "ca-app-pub-4211508479172397/8883342126"
